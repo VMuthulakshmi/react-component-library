18b85e5ea71145d581d1d7e51358dadf89fc5f84
VMuthulakshmi <vmuthulakshmi1803@gmail.com>
2023-02-23 06:48:57 +0530

    added the react component

---
+++ library/dist/esm/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function HelloWorld() {
+  return /*#__PURE__*/React.createElement("div", null, "HelloWorld");
+}
+
+export { HelloWorld };
--- library/src/components/HelloWorld/index.js
+++ library/src/components/HelloWorld/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 export default function HelloWorld() {
   return <div>HelloWorld</div>;
 }
